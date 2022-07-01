bc6bdab73f7f7328ebbdbe4e542e22442ee41296
sri-rishi <sririshi2612@gmail.com>
2022-07-01 18:26:13 +0530

    refactor: refactored code

---
+++ src/components/index/index.js
@@ -0,0 +1,15 @@
+import { Navbar } from "../Navbar/Navbar";
+import { Sidebar } from "../Sidebar/Sidebar";
+import { SearchBox } from "../Searchbox/SearchBox";
+import { RightSideBox } from "../RightSidebox/RightSideBox";
+import { Button } from "../Button/Button";
+import { FeedPostBox } from "../FeedPostBox/FeedPostBox";
+
+export {
+    Navbar,
+    Sidebar,
+    SearchBox,
+    RightSideBox,
+    Button,
+    FeedPostBox
+}
