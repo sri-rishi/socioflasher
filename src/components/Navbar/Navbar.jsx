26b471c4d15e49e48e81384a9ee8aa3d161803ec
sri-rishi <sririshi2612@gmail.com>
2022-07-01 18:22:51 +0530

    feat:a added navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+export const Navbar = () => {
+    return(
+        <nav className="nav flex-row align-items-center justify-content-evenly ">
+            <h1 className="">socio<span>Flasher</span></h1>
+            <div className="profile-img-box">
+                <img className="round-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkg3YrRehU5lvlYruB90y6K6cF4eyTsMk2iZhcG0N28iHyXrG0_VvlFeBxjcq3rhyl-Po&usqp=CAU" alt="user" />
+            </div>
+        </nav>
+    )
+}
