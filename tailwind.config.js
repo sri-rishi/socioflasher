7a3cb5ce72ed083cbd65d87c452fd8f5cc995b67
sri-rishi <sririshi2612@gmail.com>
2022-07-02 11:43:02 +0530

    chore: changed screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      "2xl":  "1535px",
+      // => @media (min-width: 1535px) { ... }
+
+      "xl": "1279px",
+      // => @media (min-width: 1279px) { ... }
+
+      "lg": "1050px",
+      // => @media (min-width: 1050px) { ... }
+
+      "md": "580px",
+      // => @media (min-width: 640px) { ... }
+
+      "sm": "300px" ,
+      // => @media (min-width: 300px) { ... }
+    },
+    extend: {
+      boxShadow: {
+        'top': "0px -1px 5px #888",
+      }
+    },
   },
   plugins: [],
 }
