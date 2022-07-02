4da8cbf6cd7c156340ebab68ebafedb4880395b5
sri-rishi <sririshi2612@gmail.com>
2022-07-02 19:10:07 +0530

    style: added width

--- src/components/FeedPost/FeedPost.jsx
+++ src/components/FeedPost/FeedPost.jsx
@@ -3,8 +3,8 @@ import { HiOutlineDotsHorizontal, RiHeart3Line, TbMessageCircle2, TbSend, IoBook
 
 export const FeedPost = () => {
     return (
-        <div className="flex flex-col mt-20 border-solid border mx-4 max-w-lg shadow-md">
-            <div className="flex flex-row items-center justify-between px-3 py-2 bg-gray-300">
+        <div className="flex flex-col border-solid border w-full max-w-lg shadow-md">
+            <div className="flex flex-row items-center justify-between px-3 py-2">
                 <div className="flex flex-row items-center gap-2.5">
                     <ProfileImage />
                     <p>User Name</p>
@@ -30,13 +30,13 @@ export const FeedPost = () => {
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi excepturi repellat sunt eius aspernatur autem! Assumenda, aliquam corporis sunt pariatur voluptatibus placeat cumque dolorum enim eos nulla adipisci rerum ipsum.
                         </span>
                     </p>
-                    <p>View all comments</p>
-                    <small>1m ago</small>
+                    <p className="text-gray-600">View all comments</p>
+                    <small className="text-gray-600">1m ago</small>
                 </div>
                 <div className="flex flex-row w-full items-center gap-2 pb-4 text-base">
                     <Button className="text-xl font-extrabold hover:text-gray-600" icon={<GrEmoji />}/>
                     <input className="w-full outline-transparent" placeholder="Add comments"/>
-                    <Button className="hover:text-sky-400" text={"Post"}/>
+                    <Button className="text-sky-600 font-semibold hover:text-sky-900" text={"Post"}/>
                 </div>
             </div>
         </div>
