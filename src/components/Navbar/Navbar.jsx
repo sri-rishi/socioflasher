7ed9c353fe45445f8d35bc897c646199f7542ed3
sri-rishi <sririshi2612@gmail.com>
2022-07-02 09:47:37 +0530

    style: styled navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,10 @@
 export const Navbar = () => {
     return(
-        <nav className="">
-            <h1 className="">socio<span>Flasher</span></h1>
+        <nav className="w-full flex flex-row items-center gap-4 py-3 px-4 bg-gray-300 fixed z-50">
+            <div className="w-10">
+                <img className="w-full rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkg3YrRehU5lvlYruB90y6K6cF4eyTsMk2iZhcG0N28iHyXrG0_VvlFeBxjcq3rhyl-Po&usqp=CAU" alt="user" />
+                </div>
+            <h1 className="text-2xl font-semibold">socio<span className="text-sky-400">Flasher</span></h1>
         </nav>
     )
 }
