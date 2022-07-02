c84dfb6a6857ce74f445009cf594b37f0522ce66
sri-rishi <sririshi2612@gmail.com>
2022-07-02 11:42:31 +0530

    style: maded responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 export const Navbar = () => {
     return(
-        <nav className="w-full flex flex-row items-center gap-4 py-3 px-4 bg-gray-300 fixed z-50">
+        <nav className="w-full flex flex-row items-center gap-4 py-3 px-4 bg-gray-300 fixed z-50 top-0 shadow md:hidden">
             <div className="w-10">
                 <img className="w-full rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkg3YrRehU5lvlYruB90y6K6cF4eyTsMk2iZhcG0N28iHyXrG0_VvlFeBxjcq3rhyl-Po&usqp=CAU" alt="user" />
                 </div>
