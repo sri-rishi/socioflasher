e8403a2f4c3e3e1829dd882ff5425a3c3a5dea69
sri-rishi <sririshi2612@gmail.com>
2022-07-03 18:27:16 +0530

    feat: added logout button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
-import { ProfileImage } from "../index"
+import { Button, ProfileImage } from "../index";
+import {FiLogOut} from "../../assests"
 
 export const Navbar = () => {
     return(
-        <nav className="w-full flex flex-row items-center gap-4 py-3 px-4 bg-gray-300 fixed z-50 top-0 shadow md:hidden">
-            <ProfileImage />
-            <h1 className="text-2xl font-semibold">socio<span className="text-sky-400">Flasher</span></h1>
+        <nav className="w-full flex flex-row items-center justify-between py-3 px-4 bg-white fixed z-50 top-0 shadow">
+            <div className="flex flex-row items-center gap-4">
+                <ProfileImage />
+                <h1 className="text-2xl font-semibold">socio<span className="text-sky-400">Flasher</span></h1>
+            </div>
+            <Button className={"text-2xl hover:text-sky-400"} icon={<FiLogOut />}/>
         </nav>
     )
 }
