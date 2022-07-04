82691a987cd6b854014a0c6d52e65acdca49ab49
sri-rishi <sririshi2612@gmail.com>
2022-07-04 12:27:05 +0530

    style: updated styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import {FiLogOut} from "../../assests"
 
 export const Navbar = () => {
     return(
-        <nav className="w-full flex flex-row items-center justify-between py-3 px-4 bg-white fixed z-50 top-0 shadow">
+        <nav className="w-full flex flex-row items-center justify-between py-3 px-4 bg-white fixed z-50 top-0 shadow md:hidden">
             <div className="flex flex-row items-center gap-4">
                 <ProfileImage />
                 <h1 className="text-2xl font-semibold">socio<span className="text-sky-400">Flasher</span></h1>
