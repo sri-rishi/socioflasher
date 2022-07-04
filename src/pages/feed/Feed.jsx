dc16bb9e0ee3c20bedd75e094c1478a87ada694d
sri-rishi <sririshi2612@gmail.com>
2022-07-04 12:28:15 +0530

    feat: added NewPostBox

--- src/pages/feed/Feed.jsx
+++ src/pages/feed/Feed.jsx
@@ -1,4 +1,4 @@
-import {  FeedPost, FollowBar, MenuBar, Navbar, Footer, FloatPostBtn} from "../../components/index/index";
+import {  FeedPost, FollowBar, MenuBar, Navbar, Footer, FloatPostBtn, NewPostBox} from "../../components/index/index";
 
 export const Feed = () => {
     return (
@@ -6,10 +6,13 @@ export const Feed = () => {
             <Navbar />
             <div className="md:flex w-full">
                 <MenuBar />
-                <main className="mt-20 md:mt-0 mb-40 md:ml-1/6 py-4 flex md:w-5/6 flex-col items-center gap-4">
-                    <FeedPost/>
-                    <FeedPost/>
-                    <FeedPost/>
+                <main className="mt-20 md:mt-0 mb-40 md:ml-1/6 py-4 flex md:w-5/6 flex-col items-center gap-4 border-x">
+                    <NewPostBox />
+                    <div className="flex flex-col gap-4 p-2">
+                        <FeedPost/>
+                        <FeedPost/>
+                        <FeedPost/>
+                    </div>
                     <FloatPostBtn />
                 </main>
                 {/* <FollowBar /> */}
