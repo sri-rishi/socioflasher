3c1a14917405fab03d0df3d52d441039dda0daca
sri-rishi <sririshi2612@gmail.com>
2022-07-04 12:29:23 +0530

    refactor: changed component  name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import { Home } from "./pages/index";
+import { Feed } from "./pages/index";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Feed />
     </div>
   );
 }
