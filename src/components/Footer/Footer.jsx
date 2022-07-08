fee691e58730403a1292160b5dbde85a2fc9e1e6
sri-rishi <sririshi2612@gmail.com>
2022-07-08 20:38:43 +0530

    feat: added NavLink

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,19 @@
+import {NavLink} from "react-router-dom"
 import { Button } from "../index";
-import {IoBookmarkOutline, GoHome, IoMdSearch, CgProfile} from "../../assests/index"
+import {IoBookmarkOutline, GoHome, IoMdSearch, CgProfile} from "../../assests/index";
 
 export const Footer = () => {
+    const getActiveStyle = ({isActive}) => ({
+        color: isActive && "rgb(56 189 248)"
+    })
     return (
         <footer className="bottom-0 fixed shadow-top bg-white flex flex-row w-full justify-around py-5 px-4 text-2xl md:hidden">
-            <Button className="hover:text-sky-400" icon={<GoHome />}/>
-            <Button className="hover:text-sky-400" icon={<IoMdSearch />}/>
+            <NavLink to="/" style={getActiveStyle}>
+                <Button className="hover:text-sky-400" icon={<GoHome />}/>
+            </NavLink>
+            <NavLink to="/explore" style={getActiveStyle}>
+                <Button className="hover:text-sky-400" icon={<IoMdSearch />}/>
+            </NavLink>
             <Button className="hover:text-sky-400" icon={<IoBookmarkOutline />}/>
             <Button className="hover:text-sky-400" icon={<CgProfile />} />
         </footer>
