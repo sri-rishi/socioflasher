354965121626cd19416f4b56565ad289be6fd672
sri-rishi <sririshi2612@gmail.com>
2022-07-09 11:27:11 +0530

    style: updated style

--- src/routes/TemplateRoute/Template.jsx
+++ src/routes/TemplateRoute/Template.jsx
@@ -8,7 +8,7 @@ export const Template = () => {
             <Navbar />
             <div className="md:flex w-full flex-row relative">
                 <MenuBar />
-                <main className="main w-full mt-16 pt-4 md:pt-0 flex flex-col items-center gap-4 border-x md:mt-0 md:w-5/6 md:ml-1/6">
+                <main className="main w-full min-h-screen mt-16 pt-4 md:pt-0 flex flex-col items-center gap-4 border-x md:mt-0 md:w-5/6 md:ml-1/6">
                     <Header />
                     <div className="w-full flex flex-col items-center pb-36 md:pb-4 gap-4"> 
                         <Outlet />
