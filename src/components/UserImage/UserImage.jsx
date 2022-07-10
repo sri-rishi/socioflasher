fd4aac4e2190330dd1695a81ec62e90cfe4238af
sri-rishi <sririshi2612@gmail.com>
2022-07-10 10:46:15 +0530

    refactor: changed src to imgSrc

--- src/components/UserImage/UserImage.jsx
+++ src/components/UserImage/UserImage.jsx
@@ -1,7 +1,10 @@
-export const UserImage = () => {
+export const UserImage = ({imgSrc}) => {
     return (
         <div className="w-10">
-            <img className="w-full rounded-full object-cover" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkg3YrRehU5lvlYruB90y6K6cF4eyTsMk2iZhcG0N28iHyXrG0_VvlFeBxjcq3rhyl-Po&usqp=CAU" alt="user" />
+            <img 
+                className="w-full rounded-full object-cover" 
+                src={imgSrc && imgSrc} 
+                alt="user" />
         </div>
     )
 }
