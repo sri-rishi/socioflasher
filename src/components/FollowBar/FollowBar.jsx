9088b514a5099c1c494adaf135ad4bb48344e66b
sri-rishi <sririshi2612@gmail.com>
2022-07-10 10:53:59 +0530

    deleted files

--- src/components/FollowBar/FollowBar.jsx
+++ src/components/FollowBar/FollowBar.jsx
@@ -1,21 +1,6 @@
 import { Button, UserImage, SearchBox } from "../index/index";
-import { useSelector, useDispatch} from "react-redux";
-import { useEffect } from "react";
-import { loadUsers } from "../../features/usersSlice";
 
 export const FollowBar = () => {
-    const {users, userStatus} = useSelector(store => store.users);
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        if(userStatus === "idle") {
-            dispatch(loadUsers());
-        }
-    }, [dispatch, userStatus])
-
-    if(userStatus === "fulfilled") {
-        console.log(users)
-    }
     return (
         <div className="hidden w-1/4 lg:flex flex-col gap-8 fixed top-0 right-0 items-start p-4 xl:px-8">
                 <div className="w-full flex flex-col items-center gap-4">
@@ -26,23 +11,34 @@ export const FollowBar = () => {
                             <div className="border-b py-2 px-1">
                                 <p>People to follow</p>
                             </div>
-                            {
-                                [...users].slice(0, 4).map(({_id, firstName, lastName, userHandler, profilePic}) => (
-                                    <div 
-                                        key={_id} 
-                                        className="flex flex-row items-center justify-between py-2 px-1 hover:bg-slate-100"
-                                    >
-                                        <div className="flex flex-row items-center gap-2">
-                                            <UserImage imgSrc={profilePic} />
-                                            <div className="flex flex-col gap-0.5">
-                                                <p className="font-semibold text-sm">{`${firstName} ${lastName}`}</p>
-                                                <p className="text-gray-400 text-xs">{userHandler}</p>
-                                            </div>
-                                        </div>
-                                        <Button className="bg-sky-600 py-1 px-3 rounded-2xl text-white hover:bg-sky-700" text={"Follow"}/>
-                                    </div>
-                                ))
-                            }
+                            <div className="flex flex-row items-center justify-between py-2 px-1 hover:bg-slate-100">
+                                <div className="flex flex-row items-center gap-2">
+                                    <UserImage />
+                                    <p className="font-semibold text-sm">User Name</p>
+                                </div>
+                                <Button className="bg-sky-600 py-1 px-3 rounded-2xl text-white hover:bg-sky-700" text={"Follow"}/>
+                            </div>
+                            <div className="flex flex-row items-center justify-between py-2 px-1 hover:bg-slate-100">
+                                <div className="flex flex-row items-center gap-2">
+                                    <UserImage />
+                                    <p className="font-semibold text-sm">User Name</p>
+                                </div>
+                                <Button className="bg-sky-600 py-1 px-3 rounded-2xl text-white hover:bg-sky-700" text={"Follow"}/>
+                            </div>
+                            <div className="flex flex-row items-center justify-between py-2 px-1 hover:bg-slate-100">
+                                <div className="flex flex-row items-center gap-2">
+                                    <UserImage />
+                                    <p className="font-semibold text-sm">User Name</p>
+                                </div>
+                                <Button className="bg-sky-600 py-1 px-3 rounded-2xl text-white hover:bg-sky-700" text={"Follow"}/>
+                            </div>
+                            <div className="flex flex-row items-center justify-between py-2 px-1 hover:bg-slate-100">
+                                <div className="flex flex-row items-center gap-2">
+                                    <UserImage />
+                                    <p className="font-semibold text-sm">User Name</p>
+                                </div>
+                                <Button className="bg-sky-600 py-1 px-3 rounded-2xl text-white hover:bg-sky-700" text={"Follow"}/>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/index.js
+++ src/index.js
@@ -4,19 +4,16 @@ import "./index.css";
 import App from "./App";
 import {BrowserRouter as Router} from "react-router-dom";
 import { makeServer } from "./server";
-import {Provider} from "react-redux";
-import { store } from "./app/store";
+
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
       <Router>
         <App />
       </Router>
-    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
