723692659ffa4b28ffa403dd65c8b60df1ab2ab7
sri-rishi <sririshi2612@gmail.com>
2022-07-12 11:24:23 +0530

    refactor: exported files

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,3 +2,4 @@ export { Feed } from "../feed/Feed";
 export {Explore} from "../explore/Explore"; 
 export {UserProfilePage} from "../userProfilePage/UserProfilePage";
 export {BookMarks} from "../bookmarks/BookMarks";
+export {Login, SignUp, } from "../auth";
