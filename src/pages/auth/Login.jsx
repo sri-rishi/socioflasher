e54cd0d6db18a49a0a75f7255e1e995a5d56a861
sri-rishi <sririshi2612@gmail.com>
2022-07-12 11:21:58 +0530

    feat: added login page and functionality

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,6 +1,30 @@
-import { Button } from "../../components/index"
+import { Button } from "../../components/index";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { loginUser } from "./authSlice";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
+    const {user, token} = useSelector(store => store?.auth);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [loginDetails, setLoginDetails] = useState({
+        email: "",
+        password: ""
+    });
+
+    const loginHandler = () => {
+        if(loginDetails.email !== "" && loginDetails.password !== "") {
+            dispatch(loginUser(loginDetails))
+        }
+
+        if(token) {
+            navigate("/")
+        }
+    };
+
+
+    
     return (
         <div className="w-full h-screen flex flex-row justify-center items-center">
             <div className="flex flex-col items-center bg-white shadow-md p-8 rounded-2xl gap-4 font-semibold border-y-8 border-sky-700">
@@ -13,17 +37,36 @@ export const Login = () => {
                         <label htmlFor="username">
                             Username or Email
                         </label>
-                        <input className="border-2 p-2 rounded font-normal" type="text" name="username" placeholder="Username Or Email"/>
+                        <input 
+                            className="border-2 p-2 rounded font-normal" 
+                            type="text" 
+                            name="username" 
+                            placeholder="Username Or Email"
+                            value={loginDetails.email}
+                            onChange={(e) => setLoginDetails(details => ({...details, email: e.target.value}))}
+                        />
                     </div>
                     <div className="w-full flex flex-col gap-2">
                         <label htmlFor="password">
                             Password
                         </label>
-                        <input className="border-2 p-2 rounded font-normal"  type="password" name="password" placeholder="Password"/>
+                        <input 
+                            className="border-2 p-2 rounded font-normal"  
+                            type="password" 
+                            name="password" 
+                            placeholder="Password"
+                            value={loginDetails.password}
+                            onChange={(e) => setLoginDetails(details => ({...details, password: e.target.value}))}
+                        />
                     </div>
                 </div>
                 <div className="w-full flex flex-col gap-4 py-2">
-                    <Button className={"bg-sky-600 p-2 rounded-2xl text-white hover:bg-sky-900"} text={"Sign In with test"}/>
+                    <Button 
+                        className={"bg-sky-600 p-2 rounded-2xl text-white hover:bg-sky-900"} 
+                        text={"Sign In"}
+                        onClick={() => loginHandler()}
+                    />
+                    <Button className={"bg-white p-2 border-sky-600 rounded-2xl text-sky-600 hover:bg-sky-900"} text={"Sign In with test"}/>
                     <Button className={"bg-white p-2 rounded-2xl text-gray-400 border-2 hover:bg-sky-600 hover:text-white hover:border-transparent"} text={"Create new account"}/>
                 </div>
             </div>
