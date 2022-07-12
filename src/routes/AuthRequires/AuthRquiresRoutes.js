7d444c6af5d5f0bdd7dc981daefd49f4d5ba0e37
sri-rishi <sririshi2612@gmail.com>
2022-07-12 11:23:21 +0530

    feat: created template for auth route

---
+++ src/routes/AuthRequires/AuthRquiresRoutes.js
@@ -0,0 +1,9 @@
+import { useSelector } from "react-redux";
+import { Navigate, useLocation } from "react-router-dom";
+
+
+export const AuthRequiresRoutes = ({children}) => {
+    const {token} = useSelector(store => store.auth)
+    const location = useLocation();
+    return token ? children : <Navigate to="/login" state={{ from: location?.pathname }} replace />;
+}
