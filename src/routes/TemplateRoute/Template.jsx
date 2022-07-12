4a261aaabb849f30cb3157fc8d3ded09229a6872
sri-rishi <sririshi2612@gmail.com>
2022-07-12 11:23:57 +0530

    refactor: replaced oulet with children

--- src/routes/TemplateRoute/Template.jsx
+++ src/routes/TemplateRoute/Template.jsx
@@ -1,7 +1,6 @@
-import { Outlet } from "react-router-dom"
 import { Header, FloatPostBtn, FollowBar, MenuBar, Navbar, Footer} from "../../components/index"
 
-export const Template = () => {
+export const Template = ({children}) => {
     return (
         <div>
             <div className="w-full">
@@ -11,7 +10,7 @@ export const Template = () => {
                 <main className="main w-full min-h-screen mt-16 pt-4 md:pt-0 flex flex-col items-center gap-4 border-x md:mt-0 md:w-5/6 md:ml-1/6">
                     <Header />
                     <div className="w-full flex flex-col items-center pb-36 md:pb-4 gap-4"> 
-                        <Outlet />
+                        {children}
                     </div>
                     <FloatPostBtn />
                 </main>
