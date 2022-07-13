5c60f5e14344035dd1816e2b843957cc2acafcfe
sri-rishi <sririshi2612@gmail.com>
2022-07-13 19:04:47 +0530

    feat: added user info to profileBox

--- src/components/ProfileBox/ProfileBox.jsx
+++ src/components/ProfileBox/ProfileBox.jsx
@@ -1,29 +1,37 @@
+import { GiBiohazard } from "react-icons/gi";
 import { Button } from "../index";
 
-export const ProfileBox = () => {
+export const ProfileBox = ({
+    imgSrc, 
+    firstName,
+    lastName,
+    userHandler,
+    bio,
+    link
+}) => {
     return (
-        <div className="flex flex-row justify-around w-full max-w-lg bg-white py-4 border rounded">
-            <div className="w-36 h-36 p-4">
-                <img className="w-full rounded-full" src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkg3YrRehU5lvlYruB90y6K6cF4eyTsMk2iZhcG0N28iHyXrG0_VvlFeBxjcq3rhyl-Po&usqp=CAU"} alt="user" />
+        <div className="flex flex-row justify-evenly w-full max-w-lg bg-white py-4 border rounded">
+            <div className="w-36 h-36 p-2">
+                <img className="w-full rounded-full" src={imgSrc && imgSrc} alt={`${firstName} pic`} />
             </div>
-            <div className="flex flex-col gap-4 p-4">
+            <div className="flex flex-col gap-8 p-2">
                 <div className="flex flex-row items-center justify-between">
                     <div className="flex flex-col">
-                        <p className="font-bold text-xl">User Name</p>
-                        <p className="text-gray-400">@user123</p>
+                        <p className="font-bold text-xl">{`${firstName} ${lastName}`}</p>
+                        <p className="text-gray-400">{`@${userHandler}`}</p>
                     </div>
                     <Button className={"bg-slate-700 px-4 py-1.5 text-white rounded hover:bg-slate-800"} text={"Edit"}/>
                 </div>
                 <p className="text-gray-700 font-medium">
-                    An aspiring web developer
+                    {bio}
                 </p>
                 <div className="flex flex-row  gap-4 text-gray-700 font-medium">
                     <p>3 posts</p>
                     <p>3 Followers</p>
                     <p>3 Following</p>
                 </div>
-                <a className="text-sky-600" href="https://neog-lms.vercel.app/docs/prds/social-media-app" target="_blank">
-                    <p>https://neog-lms.vercel.app/docs/prds/social-media-app</p>
+                <a className="text-sky-600" href="https://neog-lms.vercel.app/docs/prds/social-media-app" target="_blank" rel="noopener noreferrer">
+                    <p>{link}</p>
                 </a>
             </div>
         </div>
--- src/features/ProfilePages/ProfilePage.jsx
+++ src/features/ProfilePages/ProfilePage.jsx
@@ -1,9 +1,19 @@
-import { Post, ProfileBox } from "../../components/index/index"
+import { Post, ProfileBox } from "../../components/index/index";
+import { useSelector } from "react-redux";
 
 export const ProfilePage = () => {
+    const {user} = useSelector(store => store?.auth);
+
     return (
         <>
-            <ProfileBox />
+            <ProfileBox 
+                imgSrc={user.profilePic} 
+                firstName={user.firstName} 
+                lastName={user.lastName}
+                userHandler={user.userHandler}
+                bio={user.bio}
+                link={user.link}
+            />
             <div className="w-full flex flex-col items-center gap-4">
                 <Post />
                 <Post />
