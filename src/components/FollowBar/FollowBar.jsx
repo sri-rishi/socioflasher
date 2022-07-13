2429510b9d21cdf4c450291d201d0664e2014869
sri-rishi <sririshi2612@gmail.com>
2022-07-13 10:14:16 +0530

    feat: mapped over users data

--- src/components/FollowBar/FollowBar.jsx
+++ src/components/FollowBar/FollowBar.jsx
@@ -1,6 +1,13 @@
 import { Button, UserImage, SearchBox } from "../index/index";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 export const FollowBar = () => {
+    const {users} = useSelector(store => store?.users);
+    const {user} = useSelector(store => store?.auth);
+
+    const shownUsers = users?.filter(person => person._id !== user._id);
+
     return (
         <div className="hidden w-1/4 lg:flex flex-col gap-8 fixed top-0 right-0 items-start p-4 xl:px-8">
                 <div className="w-full flex flex-col items-center gap-4">
@@ -11,34 +18,27 @@ export const FollowBar = () => {
                             <div className="border-b py-2 px-1">
                                 <p>People to follow</p>
                             </div>
-                            <div className="flex flex-row items-center justify-between py-2 px-1 hover:bg-slate-100">
-                                <div className="flex flex-row items-center gap-2">
-                                    <UserImage />
-                                    <p className="font-semibold text-sm">User Name</p>
-                                </div>
-                                <Button className="bg-sky-600 py-1 px-3 rounded-2xl text-white hover:bg-sky-700" text={"Follow"}/>
-                            </div>
-                            <div className="flex flex-row items-center justify-between py-2 px-1 hover:bg-slate-100">
-                                <div className="flex flex-row items-center gap-2">
-                                    <UserImage />
-                                    <p className="font-semibold text-sm">User Name</p>
-                                </div>
-                                <Button className="bg-sky-600 py-1 px-3 rounded-2xl text-white hover:bg-sky-700" text={"Follow"}/>
-                            </div>
-                            <div className="flex flex-row items-center justify-between py-2 px-1 hover:bg-slate-100">
-                                <div className="flex flex-row items-center gap-2">
-                                    <UserImage />
-                                    <p className="font-semibold text-sm">User Name</p>
-                                </div>
-                                <Button className="bg-sky-600 py-1 px-3 rounded-2xl text-white hover:bg-sky-700" text={"Follow"}/>
-                            </div>
-                            <div className="flex flex-row items-center justify-between py-2 px-1 hover:bg-slate-100">
-                                <div className="flex flex-row items-center gap-2">
-                                    <UserImage />
-                                    <p className="font-semibold text-sm">User Name</p>
-                                </div>
-                                <Button className="bg-sky-600 py-1 px-3 rounded-2xl text-white hover:bg-sky-700" text={"Follow"}/>
-                            </div>
+
+                            {
+                                [...shownUsers]?.slice(0, 4).map(({_id, firstName, lastName, profilePic}) => (
+                                    <div 
+                                        key={_id}
+                                        className="flex flex-row items-center justify-between py-2 px-1 hover:bg-slate-100"
+                                    >
+                                        <div className="flex flex-row items-center gap-2">
+                                            <UserImage 
+                                                imgSrc={
+                                                    profilePic ? 
+                                                    profilePic : 
+                                                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0Xdf9OyXn9BpWL30gb6cpyLnkiCCbSaH8wVB1007o9WpYBDgb6J1_afDQTdJuqwgE3xM&usqp=CAU"
+                                                }
+                                            />
+                                            <p className="font-semibold text-sm">{`${firstName} ${lastName}`}</p>
+                                        </div>
+                                        <Button className="bg-sky-600 py-1 px-3 rounded-2xl text-white hover:bg-sky-700" text={"Follow"}/>
+                                    </div>
+                                ) )
+                            }
                         </div>
                     </div>
                 </div>
