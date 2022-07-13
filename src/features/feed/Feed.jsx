d09015fac96cb80c973e6fb5ac982e5d93dff3a9
sri-rishi <sririshi2612@gmail.com>
2022-07-13 11:48:16 +0530

    feat: used user value from auth store

--- src/features/feed/Feed.jsx
+++ src/features/feed/Feed.jsx
@@ -1,10 +1,14 @@
 import { Post, NewPostBox} from "../../components/index/index";
+import { useSelector } from "react-redux";
 import "./feed.css";
 
 export const Feed = () => {
+    const {users} = useSelector(store => store?.users);
+    const {user} = useSelector(store => store?.auth);
+
     return (
         <>
-            <NewPostBox />
+            <NewPostBox imgSrc={user.profilePic} alt={user.firstName}/>
             <div className="w-full flex flex-col items-center gap-4">
                 <Post/>
                 <Post/>
