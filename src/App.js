3f32ccfb46615ed188721c092c40bb688df128d2
sri-rishi <sririshi2612@gmail.com>
2022-07-13 10:17:10 +0530

    feat: dispatched getUsers function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { useEffect } from "react";
+import { getUsers } from "./features/ProfilePage/userSlice";
 import { AllRoutes } from "./routes/AllRoutes/Routes";
+import {useDispatch, useSelector} from "react-redux";
+
 
 function App() {
+  const dispatch = useDispatch();
+  const {token} = useSelector(store => store?.auth)
+  
+  useEffect(() => {
+    if(token) {
+      dispatch(getUsers());
+    }
+  }, [token])
+
   return (
     <div className="App bg-slate-100 min-h-screen">
       <AllRoutes />
