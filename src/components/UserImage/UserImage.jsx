1ce6cef2801d8b006e699f76f7992366ebd74a68
sri-rishi <sririshi2612@gmail.com>
2022-07-13 11:47:28 +0530

    refactor: added conditional img src

--- src/components/UserImage/UserImage.jsx
+++ src/components/UserImage/UserImage.jsx
@@ -1,10 +1,14 @@
-export const UserImage = ({imgSrc}) => {
+export const UserImage = ({imgSrc, alt}) => {
     return (
         <div className="w-10">
             <img 
                 className="w-full rounded-full object-cover" 
-                src={imgSrc && imgSrc} 
-                alt="user" />
+                src={
+                    imgSrc ? 
+                    imgSrc : 
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0Xdf9OyXn9BpWL30gb6cpyLnkiCCbSaH8wVB1007o9WpYBDgb6J1_afDQTdJuqwgE3xM&usqp=CAU"
+                } 
+                alt={alt && alt} />
         </div>
     )
 }
