7e95473252cde60085107a3d260db905a5d14988
sri-rishi <sririshi2612@gmail.com>
2022-07-14 20:06:51 +0530

    feat: created update user api function

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -3,3 +3,16 @@ import axios from "axios"
 export const getUsersData = () => {
     return axios.get("/api/users")
 };
+
+
+export const editUserDetails = (userData, token) => {
+    return axios.post(
+        "/api/users/edit", 
+        {
+           userData
+        },
+        {
+            headers: {authorization: token}
+        }
+    )
+}
