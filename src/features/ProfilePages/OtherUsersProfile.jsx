e594aa3bb67543be1af526ee9d96ccca3983d872
sri-rishi <sririshi2612@gmail.com>
2022-07-14 09:05:49 +0530

    feat: passed other users props

--- src/features/ProfilePages/OtherUsersProfile.jsx
+++ src/features/ProfilePages/OtherUsersProfile.jsx
@@ -1,9 +1,25 @@
-import { Post, ProfileBox } from "../../components/index/index"
+import { useParams } from "react-router-dom";
+import { Post, ProfileBox } from "../../components/index/index";
+import { useSelector } from "react-redux";
 
 export const OtherUsersProfile = () => {
+    const {userId} = useParams();
+    const {users} = useSelector(store => store?.users);
+
+    const otherUser = users.find(person => person._id === userId);
+
+    console.log(otherUser);
+
     return (
         <>
-            <ProfileBox />
+            <ProfileBox 
+                imgSrc={otherUser?.profilePic} 
+                firstName={otherUser?.firstName} 
+                lastName={otherUser?.lastName}
+                userHandler={otherUser?.userHandler}
+                bio={otherUser?.bio}
+                link={otherUser?.link}
+            />
             <div className="w-full flex flex-col items-center gap-4">
                 <Post />
                 <Post />
