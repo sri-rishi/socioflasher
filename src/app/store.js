ae69d46e8d347a5f850a8ec012a72287c96bff07
sri-rishi <sririshi2612@gmail.com>
2022-07-16 12:14:11 +0530

    feat: added post state

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import usersReducer from "../features/ProfilePages/userSlice";
+import postReducer from "../features/feed/postSlice";
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
-        users: usersReducer
+        users: usersReducer,
+        post: postReducer
     }
 })
