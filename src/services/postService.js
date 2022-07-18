2fcbac0355d761dbf3f6986d42c3026bf0083b02
sri-rishi <sririshi2612@gmail.com>
2022-07-18 19:06:42 +0530

    feat: created function for api call

--- src/services/postService.js
+++ src/services/postService.js
@@ -3,3 +3,15 @@ import axios from "axios"
 export const getAllPostsData = () => {
     return axios.get("/api/posts");
 }
+
+export const editPostData = (postData, token) => {
+    return axios.post(
+        `/api/posts/edit/${postData._id}`, 
+        {
+            postData
+        },
+        {
+            headers: {authorization: token}
+        }
+    )
+}
