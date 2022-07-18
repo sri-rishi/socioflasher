b6ddf36194f8b7f5416299087ecd0f4d97e15d24
sri-rishi <sririshi2612@gmail.com>
2022-07-18 10:13:10 +0530

    feat: passed data to post

--- src/features/feed/Feed.jsx
+++ src/features/feed/Feed.jsx
@@ -5,14 +5,21 @@ import "./feed.css";
 export const Feed = () => {
     const {users} = useSelector(store => store?.users);
     const {user} = useSelector(store => store?.auth);
+    const {allPosts} = useSelector(store => store?.post);
+
+    const userPosts = allPosts?.filter(person => person.username === user.username);
 
     return (
         <>
-            <NewPostBox imgSrc={user.profilePic} alt={user.firstName}/>
+            <NewPostBox imgSrc={user.profilePic} firstName={user.firstName}/>
             <div className="w-full flex flex-col items-center gap-4">
-                <Post/>
-                <Post/>
-                <Post/>
+                {
+                   userPosts.length !== 0 ?
+                   userPosts.map(post => (
+                    <Post key={post._id} post={post}/>
+                   )) :
+                   <p>Start following to see some post</p>
+                }
             </div>
         </>
     )
