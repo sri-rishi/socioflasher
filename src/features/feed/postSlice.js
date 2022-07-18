c35aea12ba982272e84bf8c27e219b5e52ebf711
sri-rishi <sririshi2612@gmail.com>
2022-07-18 19:05:46 +0530

    feat: added async thunk for edit post

--- src/features/feed/postSlice.js
+++ src/features/feed/postSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { getAllPostsData } from "../../services/postService";
+import { editPostData, getAllPostsData } from "../../services/postService";
 
 const initialState = {
     allPosts: []
@@ -11,6 +11,15 @@ export const getAllPosts = createAsyncThunk(
         const response = await getAllPostsData();
         return response.data
     }
+)
+
+export const editPost = createAsyncThunk(
+    "posts/editPost",
+    async (postData) => {
+        const token = localStorage.getItem("token");
+        const response = await editPostData(postData, token);
+        return response.data;
+    },
 )
 
 const postSlice = createSlice({
@@ -25,6 +34,17 @@ const postSlice = createSlice({
             state.allPosts = action.payload.posts;
         },
         [getAllPosts.rejected]: (state, action) => {
+            state.postStatus = "error";
+            state.error = action.payload;
+        },
+        [editPost.pending]: (state) => {
+            state.postStatus = "pending"
+        },
+        [editPost.fulfilled]: (state, action) => {
+            state.postStatus = "fulfilled";
+            state.allPosts = action.payload.posts;
+        },
+        [editPost.rejected]: (state, action) => {
             state.postStatus = "error";
             state.error = action.payload;
         }
