6b21bd7fcdd19167cafc6d08ba0371e7f3e80405
sri-rishi <sririshi2612@gmail.com>
2022-07-19 11:31:11 +0530

    feat: added single post functionality

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,8 +1,9 @@
 import { Button, EditPostModal, UserImage } from "../index/index";
-import { RiHeart3Line, IoBookmarkOutline, BsThreeDots} from "../../assests";
+import { RiHeart3Line, IoBookmarkOutline, BsThreeDots, TbMessageCircle2} from "../../assests";
 import { useDispatch, useSelector } from "react-redux";
-import { useState } from "react";
-import { deletePost } from "../../features/feed/postSlice";
+import { useEffect, useState } from "react";
+import { deletePost, getSinglePost } from "../../features/feed/postSlice";
+import { Link } from "react-router-dom";
 
 export const Post = ({post}) => {
     const {
@@ -29,85 +30,108 @@ export const Post = ({post}) => {
         setShowEditPost(dispaly => !dispaly);
         setShowPostMenu(dispaly => !dispaly);
     }  
+
+    useEffect(() => {
+        dispatch(getSinglePost(_id));
+    }, [_id])
     
     return (
-        <div className="flex flex-col border-solid border w-full gap-4 bg-white max-w-xl rounded py-4">
-            <div className="flex flex-row items-center justify-between px-3">
-                <div className="flex flex-row items-center gap-2.5">
-                    <UserImage imgSrc={userDetails?.profilePic} width={"w-14"}/>
-                    <div className="flex flex-col">
-                        <div className="flex flex-row items-center gap-1.5">
-                            <p className="text-lg font-medium">{`${userDetails?.firstName} ${userDetails.lastName}`}</p>
-                            <p className="font-medium text-gray-400">@{userDetails?.userHandler}</p>
+        <Link className="w-full items-center flex flex-col" to={`/post/${_id}`}>
+            <div className="flex flex-col border w-full gap-4 bg-white max-w-xl rounded py-4">
+                <div className="flex flex-row items-center justify-between px-3">
+                    <div className="flex flex-row items-center gap-2.5">
+                        <UserImage imgSrc={userDetails?.profilePic} width={"w-14"}/>
+                        <div className="flex flex-col">
+                            <Link 
+                                to={
+                                    userDetails?.username === user?.username ? 
+                                    "/profile" : 
+                                    `/userProfile/${userDetails._id}`
+                                }
+                            >
+                                <div className="flex flex-row items-center gap-1.5">
+                                    <p className="text-lg font-medium">{`${userDetails?.firstName} ${userDetails.lastName}`}</p>
+                                    <p className="font-medium text-gray-400">@{userDetails?.userHandler}</p>
+                                </div>
+                            </Link>
+                            <small className="text-gray-600">1m ago</small>
                         </div>
-                        <small className="text-gray-600">1m ago</small>
+                    </div>
+                    
+                    {
+                        userDetails.username === user.username ? 
+                        <div className="relative">
+                            <Button 
+                                className={"rounded-full p-2 text-xl text-gray-400 hover:bg-gray-400 hover:text-white"} 
+                                icon={<BsThreeDots />}
+                                onClick={() => setShowPostMenu(dispaly => !dispaly)}
+                            />
+                            <ul className={`${showPostMenu ? "flex" : "hidden"} flex-col items-center absolute top-full right-4 font-semibold text-gray-500 bg-white border border-gray-400`}>
+                                <li 
+                                    className="px-4 py-2 border-b border-gray-400 hover:text-gray-700 hover:cursor-pointer"
+                                    onClick={() => editMenuHandler()}
+                                >
+                                    Edit 
+                                </li>
+                                <li 
+                                    className="px-4 py-2 text-red-600 hover:cursor-pointer"
+                                    onClick={() => dispatch(deletePost(_id))}
+                                >
+                                    Delete
+                                </li>
+                            </ul>
+                        </div>
+                        : <></>
+                    }
+                    <div 
+                        className={
+                            `${showEditPost ? "flex" : "hidden"} w-full flex-row items-center justify-center fixed inset-0 z-40 bg-gray-50 bg-opacity-50`
+                        }
+                    >
+                        <EditPostModal setShowEditPost={setShowEditPost} post={post}/>
                     </div>
                 </div>
-                
-                {
-                    userDetails.username === user.username ? 
-                    <div className="relative">
-                        <Button 
-                            className={"rounded-full p-2 text-xl text-gray-400 hover:bg-gray-400 hover:text-white"} 
-                            icon={<BsThreeDots />}
-                            onClick={() => setShowPostMenu(dispaly => !dispaly)}
-                        />
-                        <ul className={`${showPostMenu ? "flex" : "hidden"} flex-col items-center absolute top-full right-4 font-semibold text-gray-500 bg-white border border-gray-400`}>
-                            <li 
-                                className="px-4 py-2 border-b border-gray-400 hover:text-gray-700 hover:cursor-pointer"
-                                onClick={() => editMenuHandler()}
+                <div className="flex flex-col gap-4 px-3">
+                    {
+                        media ?
+                        <div className="w-full px-8 py-4">
+                            <img className="w-full object-cover" src={media} alt="feed cover" />
+                        </div>
+                        : <></>
+                    }
+                    <div className="text-left flex flex-col gap-2 px-3">
+                        {
+                            !showFullContent && content.length > 250?
+                            <p>
+                                {[...content].slice(0, 250)}... 
+                            </p>
+                            : content 
+                        }
+                        {
+                            content.length > 250 &&
+                            <span 
+                                className="text-gray-400 hover:cursor-pointer" 
+                                onClick={() => setShowFullContent(fullContent => !fullContent)}
                             >
-                                Edit 
-                            </li>
-                            <li 
-                                className="px-4 py-2 text-red-600 hover:cursor-pointer"
-                                onClick={() => dispatch(deletePost(_id))}
-                            >
-                                Delete
-                            </li>
-                        </ul>
+                                {showFullContent ? "Show less": "Show more"}
+                            </span>
+                        }
                     </div>
-                    : <></>
-                }
-                <div 
-                    className={`${showEditPost ? "flex" : "hidden"} w-full flex-row items-center justify-center fixed inset-0 z-40 bg-gray-50 bg-opacity-50`}
-                >
-                    <EditPostModal setShowEditPost={setShowEditPost} post={post}/>
+                    
+                    <div className="flex flex-row items-center justify-around px-3">
+                        <div className="flex flex-row items-center gap-2">
+                            <Button className={"hover:text-gray-600 text-xl"} icon={<RiHeart3Line />}/>
+                            <p>{likeCount > 0 && likeCount}</p>
+                        </div>
+                        <Button className={"hover:text-gray-600 text-xl font-black"} icon={<IoBookmarkOutline />}/>
+
+                        <div className="flex flex-row items-center gap-2">
+                            <Button className={"hover:text-gray-600 text-xl"} icon={<TbMessageCircle2 />} />
+                            <p>{comments.length > 0 && comments.length}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="flex flex-col gap-4 px-3">
-                {
-                    media ?
-                    <div className="w-full px-8 py-4">
-                        <img className="w-full object-cover" src={media} alt="feed cover" />
-                    </div>
-                    : <></>
-                }
-                <div className="text-left flex flex-col gap-2 px-3">
-                    {
-                        !showFullContent && content.length > 250?
-                        <p>
-                            {[...content].slice(0, 250)}... 
-                        </p>
-                        : content 
-                    }
-                    {
-                        content.length > 250 &&
-                        <span 
-                            className="text-gray-400 hover:cursor-pointer" 
-                            onClick={() => setShowFullContent(fullContent => !fullContent)}
-                        >
-                            {showFullContent ? "Show less": "Show more"}
-                        </span>
-                    }
-                </div>
-                <div className="flex flex-row justify-between items-center p-3 text-xl">
-                    <div className="flex gap-8 flex-row items-center">
-                        <Button className={"hover:text-gray-600"} icon={<RiHeart3Line />} />
-                        <Button className={"hover:text-gray-600"} icon={<IoBookmarkOutline className="font-extrabold "/>}/>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </Link>
     )
 }
