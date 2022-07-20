2ef78dd2dd0831b2947caec4c2122159a3566c98
sri-rishi <sririshi2612@gmail.com>
2022-07-20 19:15:23 +0530

    feat: added openNewPostModal function

--- src/components/FloatPostBtn/FloatPostBtn.jsx
+++ src/components/FloatPostBtn/FloatPostBtn.jsx
@@ -1,8 +1,18 @@
 import { Button } from "../index";
 import { GiFeather } from "../../assests";
+import { openNewPostModal } from "../NewPostBox/newPostBoxSlice";
+import { useDispatch } from "react-redux";
 
-export const FloatPostBtn = ({onClick}) => {
+export const FloatPostBtn = () => {
+    const dispatch = useDispatch();
+    
     return (
-        <Button onClick={() => onClick && onClick()} className={`md:hidden fixed z-30 bottom-20 right-6 rounded-full w-14 h-14 bg-sky-600 flex flex-row items-center justify-center text-xl text-white shadow-all`} icon={<GiFeather />}/>
+        <Button 
+            onClick={() => dispatch(openNewPostModal())} 
+            className={
+                `md:hidden fixed z-30 bottom-20 right-6 rounded-full w-14 h-14 bg-sky-600 flex flex-row items-center justify-center text-xl text-white shadow-all`
+            } 
+            icon={<GiFeather />}
+        />
     )
 }
