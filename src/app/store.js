b34e1ed9c9457632d49d2a10e7c645fd4071568f
sri-rishi <sririshi2612@gmail.com>
2022-07-20 19:14:43 +0530

    feat: added newPostModal state

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import {configureStore} from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import usersReducer from "../features/ProfilePages/userSlice";
 import postReducer from "../features/feed/postSlice";
+import newPostModalReducer from "../components/NewPostBox/newPostBoxSlice";
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         users: usersReducer,
-        post: postReducer
+        post: postReducer,
+        newPostModal: newPostModalReducer
     }
 })
