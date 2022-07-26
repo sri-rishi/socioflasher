5165ce1a609f4c492902ae04529f72df6abff2bc
sri-rishi <sririshi2612@gmail.com>
2022-07-26 20:19:44 +0530

    refactor: changed file name

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
 import {configureStore} from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
-import usersReducer from "../features/ProfilePages/userSlice";
+import usersReducer from "../features/profilePages/userSlice";
 import postReducer from "../features/feed/postSlice";
 import newPostModalReducer from "../components/NewPostBox/newPostBoxSlice";
 
