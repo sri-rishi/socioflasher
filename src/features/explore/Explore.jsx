2e6dffb5bee35adcc15462c4106d37c6c0fad0f4
sri-rishi <sririshi2612@gmail.com>
2022-07-28 10:22:46 +0530

    style: update styling

--- src/features/explore/Explore.jsx
+++ src/features/explore/Explore.jsx
@@ -1,19 +1,19 @@
 import { Post, Button} from "../../components/index";
 import { IoMdSearch } from "../../assests";
 import { useSelector } from "react-redux";
-import { useEffect } from "react";
 
 export const Explore = () => {
-    const {user, token} = useSelector(store => store?.auth);
+    const {user} = useSelector(store => store?.auth);
     const {allPosts} = useSelector(store => store?.post);
 
     const postToShown = allPosts?.filter(post => post.username !== user.username);
 
-    
     return (
         <>  
-            <div className="flex flex-row items-center p-2 border-2 border-slate-200 rounded-md bg-white max-w-lg w-full gap-2 lg:hidden">
-                <Button className={"text-2xl"} icon={<IoMdSearch className=""/>} />
+            <div 
+                className="flex flex-row items-center p-2 border-2 border-slate-200 rounded-md bg-white max-w-xl w-full gap-2 lg:hidden"
+            >
+                <Button className={"text-2xl"} icon={<IoMdSearch />} />
                 <input className="outline-transparent w-full" placeholder="Search Posts, People, anything"/>
             </div>
             <div className="w-full flex flex-col items-center gap-4">
