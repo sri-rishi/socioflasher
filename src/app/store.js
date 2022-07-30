630c041a5b951b94c4e25504302cf629864536db
sri-rishi <sririshi2612@gmail.com>
2022-07-30 16:09:05 +0530

    feat: added bookmarks state

--- src/app/store.js
+++ src/app/store.js
@@ -3,12 +3,14 @@ import authReducer from "../features/auth/authSlice";
 import usersReducer from "../features/ProfilePages/userSlice";
 import postReducer from "../features/feed/postSlice";
 import newPostModalReducer from "../components/NewPostBox/newPostBoxSlice";
+import bookmarkReducer from "../features/bookmarks/bookmarksSlice";
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         users: usersReducer,
         post: postReducer,
-        newPostModal: newPostModalReducer
+        newPostModal: newPostModalReducer,
+        bookmarks: bookmarkReducer,
     }
 })
