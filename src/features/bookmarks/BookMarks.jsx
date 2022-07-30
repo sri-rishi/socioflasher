93874acd00e339047175e98f4339554987402e2a
sri-rishi <sririshi2612@gmail.com>
2022-07-30 16:11:10 +0530

    feat: added jsx for bookmarks posts

--- src/features/bookmarks/BookMarks.jsx
+++ src/features/bookmarks/BookMarks.jsx
@@ -1,7 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getBookmarks } from "./bookmarksSlice";
+import {Post} from "../../components/Post/Post";
+
 export const BookMarks = () => {
+    const {bookmarkPosts} = useSelector(store => store?.bookmarks)
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getBookmarks());
+    }, [bookmarkPosts])
+    
     return (
-        <>
-            <p className="text-2xl">You haven't bookmarked any post</p>
+        <> 
+            {
+                bookmarkPosts.length !== 0 ?
+                bookmarkPosts.map((post) => (
+                    <Post key={post._id} post={post}/>
+                ))
+                :
+                <p className="text-2xl">You haven't bookmarked any post</p>
+            }
         </>
     )
 }
