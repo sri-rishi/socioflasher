6c6fc0039cafc2ef28fad071fcfa621383e889ec
sri-rishi <sririshi2612@gmail.com>
2022-07-30 16:12:30 +0530

    refactor: refactored code

--- src/features/feed/Feed.jsx
+++ src/features/feed/Feed.jsx
@@ -18,9 +18,16 @@ export const Feed = () => {
             >
                 <div className="flex fle-row items-center gap-4">
                     <UserImage imgSrc={user.profilePic} alt={user.firstName} width={"w-10"}/>
-                    <input className="focus:outline-transparent w-full bg-transparent hover:cursor-text" placeholder={`What's on your mind, ${user.firstName}`} disabled/>
+                    <input 
+                        className="focus:outline-transparent w-full bg-transparent hover:cursor-text" 
+                        placeholder={`What's on your mind, ${user.firstName}`} 
+                        disabled
+                    />
                 </div>
-                <Button className={"font-semibold bg-sky-600 py-1 px-6 rounded-2xl text-white hover:bg-sky-700"} text={"Add"}/>
+                <Button 
+                    className={"font-semibold bg-sky-600 py-1 px-6 rounded-2xl text-white hover:bg-sky-700"} 
+                    text={"Add"}
+                    />
             </div>
             <div className="w-full flex flex-col items-center gap-4">
                 {
