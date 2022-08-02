f142509ef765c783225fc690c36a628f93469429
sri-rishi <sririshi2612@gmail.com>
2022-08-02 11:00:59 +0530

    feat: added un/follow functionality

--- src/components/FollowBar/FollowBar.jsx
+++ src/components/FollowBar/FollowBar.jsx
@@ -1,12 +1,32 @@
 import { Button, UserImage, SearchBox } from "../index/index";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { followUnfollowUser } from "../../features/profilePages/userSlice";
+import { useEffect, useState } from "react";
 
 export const FollowBar = () => {
     const {users} = useSelector(store => store?.users);
     const {user} = useSelector(store => store?.auth);
+    const dispatch = useDispatch();
+    const [shownUsers, setShownUsers] = useState([])
 
-    const shownUsers = users?.filter(person => person._id !== user._id);
+
+    useEffect(() => {
+        setShownUsers(
+            users
+            .filter((person) => person.username !== user.username)
+            .filter((person) => !user.following.find((person2) => person2._id === person._id))
+            .slice(0, 4)
+        )
+    }, [user, users])
+    
+    const followHandler = (id) => {
+        dispatch(followUnfollowUser({
+            followUserId: id, 
+            dispatch: dispatch, 
+            isFollowed: false
+        }))
+    }
 
     return (
         <div className="hidden w-1/4 lg:flex flex-col gap-8 fixed top-0 right-0 items-start p-4 xl:px-8">
@@ -19,8 +39,9 @@ export const FollowBar = () => {
                                 <p>People to follow</p>
                             </div>
 
-                            {
-                                [...shownUsers]?.slice(0, 4).map(({_id, firstName, lastName, profilePic}) => (
+                            {   
+                            shownUsers.length > 0 ?
+                                shownUsers.map(({_id, firstName, lastName, profilePic}) => (
                                     <div 
                                         key={_id}
                                         className="flex flex-row items-center justify-between py-2 px-1 hover:bg-slate-100"
@@ -39,9 +60,14 @@ export const FollowBar = () => {
                                                 <p className="font-semibold text-sm">{`${firstName} ${lastName}`}</p>
                                             </Link>
                                         </div>
-                                        <Button className="bg-sky-600 py-1 px-3 rounded-2xl text-white hover:bg-sky-700" text={"Follow"}/>
+                                        <Button 
+                                            className="bg-sky-600 py-1 px-3 rounded-2xl text-white hover:bg-sky-700" 
+                                            text={"Follow"}
+                                            onClick={() => followHandler(_id)}
+                                        />
                                     </div>
                                 ) )
+                                : <></>
                             }
                         </div>
                     </div>
