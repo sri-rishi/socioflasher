41ac7b9d0cf02ef97e4302428b742a0d7a14d64f
sri-rishi <sririshi2612@gmail.com>
2022-08-03 10:27:32 +0530

    feat: made header dynamic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,18 @@
-export const Header = () => {
+import { useEffect } from "react";
+
+export const Header = ({path}) => {
+    const upperCaseText = path.slice(1, 2).toUpperCase();
+    const lowerCaseText = path.slice(2);
     return (
         <header className="hidden md:flex border-b w-full p-4 text-left sticky top-0 left-0 bg-slate-100">
-            <p className="text-lg font-semibold">Home</p>
+            <p className="text-lg font-semibold">
+                {
+                    path === "/" ? 
+                    "Home" : 
+                    path.split("").includes("4") ?
+                    "Profile" : 
+                    upperCaseText + lowerCaseText}
+                </p>
         </header>
     )
 }
