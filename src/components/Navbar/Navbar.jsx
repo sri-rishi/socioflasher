8727f498d4d2594b9d13e966ba83da512ea8b79a
sri-rishi <sririshi2612@gmail.com>
2022-08-03 20:02:32 +0530

    feat: added link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import { Button, UserImage } from "../index";
 import {FiLogOut} from "../../assests"
 import { useDispatch, useSelector } from "react-redux";
 import { logoutUser } from "../../features/auth/authSlice";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
     const dispatch = useDispatch();
@@ -10,7 +11,9 @@ export const Navbar = () => {
         <nav className="w-full flex flex-row items-center justify-between py-3 px-4 bg-white fixed z-50 top-0 shadow md:hidden">
             <div className="flex flex-row items-center gap-4">
                 <UserImage imgSrc={user.profilePic} alt={user.firstName} width={"w-10"}/>
-                <h1 className="text-2xl font-semibold">socio<span className="text-sky-400">Flasher</span></h1>
+                <Link to="/">
+                    <h1 className="text-2xl font-semibold">socio<span className="text-sky-400">Flasher</span></h1>
+                </Link>
             </div>
             <Button 
                 className={"text-2xl hover:text-sky-400"}  
